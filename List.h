7d24a1c7374cf56f339741aea071c756646761fa
dnspopagat@gmail.com <dnspopagat@gmail.com>
2023-08-12 10:32:09 +0800

    implement push back

--- List.h
+++ List.h
@@ -7,29 +7,57 @@
 #include <memory>
 
 
-template <class _Type, 
+template <class _Type, class T = unsigned long long,
 	class Allocator = std::allocator<_Type>>
 class List
 {
 	using _Traits = std::allocator_traits<Allocator>;
+	using Type_size = T;
+
+	Allocator alloc_1;
+	Type_size size;
 
 public:
-	List() = default;
+	List() : first(nullptr), last(nullptr) {
+		// using without allocator
+		// this->node = _Traits::allocate(alloc_1, sizeof(Node<_Type>));
+		size = 0;
+	}
 	~List() = default;
+
+	// methods to work with list
+	// _Type* get_value_of() const noexcept { return this->node->data; }
+	bool is_empty() { return first == nullptr; }
+	void push_back(_Type data);
 
 private:
 
+	// Correct implements of the node in the list 
 	template <class _Type> class Node {
-		Node* next;
-
-		typename _Type data;
 	public:
-		Node() = default;
+		Node* next;
+		typename _Type data;
+
+		Node() : data(_Type()), next(nullptr) { };
+		Node(const _Type& data) : data(data), next(nullptr) { };
+		Node(_Type& data) : data(data), next(nullptr) { };
+
 		~Node() = default;
 
 	};
 
-	Node<_Type>* node;
+	Node<_Type>* first;
+	Node<_Type*> last;
 };
 
+
+template<class _Type, class T, 
+	class Allocator>
+inline void List<_Type, T, Allocator>::push_back(_Type data)
+{
+	Node<_Type>* p_node = new Node<_Type>(data);
+}
+
+
 #endif // _list
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "List.h"
+#include <iostream>
 
 
 int main(int argc, char** argv)
 {
 	List<int> list;
+	if (list.is_empty()) std::cout << "List is empty!\n";
+	list.push_back(10);
+
 	return 0;
 }
